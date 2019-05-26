99c03e05055483ba08dbf32e2d5f6560b31d6068
Tony Tran <s3604730@student.rmit.edu.au>
2019-05-26 17:23:12 +1000

    Revert "sphinx"
    This reverts commit 39d6540a2ea4eaf69a6c87fe1f62e7ea7d57d959.

--- Calendar.py
+++ Calendar.py
@@ -7,15 +7,9 @@ from oauth2client import file, client, tools
 
 
 class Calendar:
-<<<<<<< HEAD
-    def __init__(self):
-        SCOPES = "https://www.googleapis.com/auth/calendar"
-        store = file.Storage("token.json")
-=======
     def addEvent(self, startDate, endDate, bookID):
         SCOPES = 'https://www.googleapis.com/auth/calendar'
         store = file.Storage('storage.json')
->>>>>>> parent of 39d6540... sphinx
         creds = store.get()
         if(not creds or creds.invalid):
             flow = client.flow_from_clientsecrets("credentials.json", SCOPES)
@@ -23,43 +17,34 @@ class Calendar:
         self.service = build("calendar", "v3", credentials=creds)
 
     def addEvent(self,bookID,userName):
-        borrowDate = datetime.now()
-        returnDate = borrowDate + timedelta(days=7)
-        borrowDateStr = borrowDate.strftime("%Y-%m-%d")
-        returnDateStr = returnDate.strftime("%Y-%m-%d")
-        bookReturnStart = "{}T23:59:00+10:00".format(borrowDateStr)
-        bookReturnDueDate = "{}T23:59:00+10:00".format(returnDateStr)
-        event = {
-            "summary": "Book ID: " + bookID +" User Name: " + userName,
-            "description": "New Book Return Date",
-            "start": {
-                "dateTime": bookReturnStart,
-                "timeZone": "Australia/Melbourne",
-            },
-            "end": {
-                "dateTime": bookReturnDueDate,
-                "timeZone": "Australia/Melbourne",
-            },
-            "reminders": {
-                "useDefault": False,
-                "overrides": [
-                    { "method": "email", "minutes": 120 },
-                    { "method": "popup", "minutes": 120 },
-                ],
-            }
-        }
-        event = self.service.events().insert(calendarId = "primary", body = event).execute()
-        print("Book return date added to calendar")
+        """ 
+        Adds an event to the Calendar of the library for 7 days when a book is borrowed
+        with details of 
+            param1 'BookID' of the borrowed book. 
+            param2: 'UserId' of the user who borrowed the book
+            param3: 'Start' date when the book is borrowed
+            param4: 'End' date when the book has to be returned
+        
+        then prints " Event added " message on the console. 
+        """
+        SCOPES = 'https://www.googleapis.com/auth/calendar'
+        store = file.Storage('storage.json')
+        creds = store.get()
+        if not creds or creds.invalid:
+            flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+            creds = tools.run_flow(flow, store)
+        GCAL = discovery.build('calendar', 'v3', http=creds.authorize(Http()))
 
-<<<<<<< HEAD
-    def removeEvent(self,bookID):
-        listEvents = self.service.events().list(calendarId = "primary", q = bookID).execute()
-        getEvents = listEvents.get("items", [])
-        for event in getEvents:
-            eventId = event['id']
-        event = self.service.events().delete(calendarId='primary', eventId= eventId).execute()
-        print('Event removed from calendar')
-=======
+        EVENT = {
+            'summary': 'Book Return Date',
+            'bookID': {bookID},
+            'Start':  {'dateTime': startDate, },
+            'End':    {'dateTime': endDate, },
+        }
+        e = GCAL.events().insert(calendarId='primary',
+                                 sendNotifications=True, body=EVENT).execute()
+        print("Event added")
+
     def removeEvent(self):
         SCOPES = 'https://www.googleapis.com/auth/calendar'
         store = file.Storage('storage.json')
@@ -70,4 +55,3 @@ class Calendar:
         GCAL = discovery.build('calendar', 'v3', http=creds.authorize(Http()))
         e = GCAL.events().delete(calendarId='primary', eventId='eventId').execute()
         print('Book returned')
->>>>>>> parent of 39d6540... sphinx
